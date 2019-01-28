1d3ef12f9f970a92844d6144dc3178367e4e3030
i.interspace <i.interspace@yandex.ru>
2019-01-28 15:27:21 +0300

    init

---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+
+gulp.task('sass', function () {
+    return gulp.src('./src/styles/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssmin())
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('pug', function() {
+    return gulp.src("./src/*.pug")
+        .pipe(pug())
+        .pipe(gulp.dest("./dist/"))
+        .pipe(reload({stream:true}));
+});
+
+var scripts = ['./src/scripts/*.js'];
+
+gulp.task('scripts', function() {
+    return gulp.src(scripts)
+        .pipe(concat('app.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('watch', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+  gulp.watch(['./src/styles/**.scss',
+      './src/styles/**/*.scss'], ['sass']);
+  gulp.watch('./src/**/*.pug', ['pug']);
+  gulp.watch(['./src/images/*.*', './src/images/**/*.*'], ['images']);
+  gulp.watch(scripts, ['scripts']);
+});
+
+gulp.task('build', [
+    'sass', 'pug', 'scripts', 'images'
+]);
+
